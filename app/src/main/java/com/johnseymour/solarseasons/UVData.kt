a287154c1ccb376161c608c1052143f3463e1b52
John Seymour <john.seymour787@gmail.com>
2021-06-19 18:47:53 +1000

    Added more details to UVData and updated deserialiser to handle this.

--- app/src/main/java/com/johnseymour/solarseasons/UVData.kt
+++ app/src/main/java/com/johnseymour/solarseasons/UVData.kt
@@ -1,11 +1,44 @@
 package com.johnseymour.solarseasons
 
 import android.os.Parcelable
-import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 import java.time.ZonedDateTime
 
 @Parcelize
 data class UVData(val uv: Float,
-                 // val uvTime: ZonedDateTime,
-                 @SerializedName("uv_max") val uvMax: Float): Parcelable
+                  val uvTime: ZonedDateTime? = null,
+                  val uvMax: Float? = null,
+                  val uvMaxTime: ZonedDateTime? = null,
+                  val ozone: Float? = null,
+                  val ozoneTime: ZonedDateTime? = null,
+                  val safeExposure: Map<String, Int>? = null): Parcelable
+{
+    val colorInt: Int
+    get() =
+        if (uv < 3.0)
+        {
+            R.color.uv_low
+        }
+        else if (uv >= 3.0 && uv < 6.0)
+        {
+            R.color.uv_moderate
+        }
+        else if (uv >= 6.0 && uv < 8.0)
+        {
+            R.color.uv_high
+        }
+        else if (uv >= 8.0 && uv < 11.0)
+        {
+            R.color.uv_very_high
+        }
+        else
+        {
+            R.color.uv_extreme
+        }
+
+    companion object
+    {
+        const val UV_DATA_CHANGED = "com.johnseymour.solarseasons.UVDATA_CHANGED"
+        const val UV_DATA_KEY = "uv_data_key"
+    }
+}
--- app/src/main/java/com/johnseymour/solarseasons/api/UVDataDeserialiser.kt
+++ app/src/main/java/com/johnseymour/solarseasons/api/UVDataDeserialiser.kt
@@ -1,27 +1,65 @@
 package com.johnseymour.solarseasons.api
 
-import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
+import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 import com.johnseymour.solarseasons.UVData
 import java.lang.reflect.Type
+import java.time.Instant
+import java.time.ZoneId
 
 object UVDataDeserialiser: JsonDeserializer<UVData>
 {
-    override fun deserialize(
-        json: JsonElement?,
-        typeOfT: Type?,
-        context: JsonDeserializationContext?
-    ): UVData
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): UVData
     {
         json?.asJsonObject?.getAsJsonObject("result")?.let()
         {
             val uv = it.getAsJsonPrimitive("uv")?.asFloat ?: 0F
-            val maxUV = it.getAsJsonPrimitive("uv_max")?.asFloat ?: 0F
+            val uvMax = it.getAsJsonPrimitive("uv_max")?.asFloat ?: 0F
+            val ozone = it.getAsJsonPrimitive("ozone")?.asFloat ?: 0F
 
-            return UVData(uv, maxUV)
+            val uvTimeString = it.getAsJsonPrimitive("uv_time")?.asString ?: ""
+            val uvMaxTimeString = it.getAsJsonPrimitive("uv_max_time")?.asString ?: ""
+            val ozoneTimeString = it.getAsJsonPrimitive("ozone_time")?.asString ?: ""
+
+            val uvTime = Instant.parse(uvTimeString).atZone(ZoneId.systemDefault())
+            val uvMaxTime = Instant.parse(uvMaxTimeString).atZone(ZoneId.systemDefault())
+            val ozoneTime = Instant.parse(ozoneTimeString).atZone(ZoneId.systemDefault())
+
+            val safeExposure = it.getAsJsonObject("safe_exposure_time")?.let()
+            { safeExposureObject ->
+                getSafeExposureTimes(safeExposureObject)
+            }
+
+            return UVData(uv, uvTime, uvMax, uvMaxTime, ozone, ozoneTime, safeExposure)
         }
 
-        return UVData(40F, 40F)
+        return UVData(40F)
+    }
+
+    /**
+     * Converts a JSONObject to a Kotlin Map for the safe exposure times data.
+     * @param exposuresJSON - A GSON JsonObject with string keys and all values as integers
+     * @return - Kotlin Map<String, Int> with matching string keys and Kotlin Int type values. If
+     *            the JSON value is null, it is not added to the map. Additionally, if all values
+     *            in the JSON are null, a null Kotlin Map is returned.
+     */
+    private fun getSafeExposureTimes(exposuresJSON: JsonObject): Map<String, Int>?
+    {
+        val result = mutableMapOf<String, Int>()
+
+        for (key in exposuresJSON.keySet())
+        {
+            //Can be JSON null
+            val exposureValue = exposuresJSON.get(key)
+
+            if (!exposureValue.isJsonNull)
+            {
+                exposureValue.asInt.let { result[key] = it }
+            }
+        }
+
+        return if (result.isNotEmpty()) { result } else { null }
     }
 }
