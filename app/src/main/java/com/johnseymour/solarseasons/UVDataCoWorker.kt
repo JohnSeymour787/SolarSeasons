96954cba5107dbec8684d1cafdfcc39474514e4e
John Seymour <john.seymour787@gmail.com>
2021-11-05 23:20:30 +1100

    Fixed worker not running on low battery for immediate requests

--- app/src/main/java/com/johnseymour/solarseasons/UVDataCoWorker.kt
+++ app/src/main/java/com/johnseymour/solarseasons/UVDataCoWorker.kt
@@ -228,14 +228,14 @@ class UVDataWorker(applicationContext: Context, workerParameters: WorkerParamete
                 return uvDataDeferred?.promise
             }
 
-
         private fun createWorkRequest(delayedStart: Boolean): OneTimeWorkRequest
         {
-            return OneTimeWorkRequestBuilder<UVDataWorker>().setConstraints(workConstraints).run()
+            return OneTimeWorkRequestBuilder<UVDataWorker>().run()
             {
                 if (delayedStart)
                 {
                     setInitialDelay(1, TimeUnit.MINUTES)
+                    setConstraints(workConstraints) // Only set the battery constraint when doing a delayed (background) start
                 }
                 build()
             }
@@ -260,46 +260,36 @@ class UVDataWorker(applicationContext: Context, workerParameters: WorkerParamete
             }
 
             // Start a unique work, but if one is already going, then keep that one (shouldn't need to occur because removed the work before)
-            uvDataRequest?.let {workManager.enqueueUniqueWork(WORK_NAME, ExistingWorkPolicy.KEEP, it) }
+            uvDataRequest?.let { workManager.enqueueUniqueWork(WORK_NAME, ExistingWorkPolicy.KEEP, it) }
 
             return workManager.getWorkInfosForUniqueWorkLiveData(WORK_NAME)
         }
     }
 
+
+    val test = UVData(uv=0.0399F, uvTime= ZonedDateTime.parse("2021-09-25T00:00:30.826+10:00[Australia/Sydney]"),
+        uvMax=3.0005F, uvMaxTime= ZonedDateTime.parse("2021-09-25T21:53:36.274+10:00[Australia/Sydney]"),
+        ozone=332.5F, ozoneTime= ZonedDateTime.parse("2021-09-25T16:04:07.137+10:00[Australia/Sydney]"),
+        safeExposure= mapOf("st1" to 4180, "st2" to 5016, "st3" to 6688, "st4" to 8360, "st5" to 13376, "st6" to 25079),
+        sunInfo=SunInfo(solarNoon= ZonedDateTime.parse("2021-09-25T21:53:36.274+10:00[Australia/Sydney]"), nadir=ZonedDateTime.parse("2021-09-25T09:53:36.274+10:00[Australia/Sydney]"),
+            sunrise=ZonedDateTime.parse("2021-09-25T15:52:48.317+10:00[Australia/Sydney]"),
+            sunset=ZonedDateTime.parse("2021-09-26T03:54:24.230+10:00[Australia/Sydney]"),
+            sunriseEnd=ZonedDateTime.parse("2021-09-25T15:56:13.870+10:00[Australia/Sydney]"),
+            sunsetStart=ZonedDateTime.parse("2021-09-26T03:50:58.677+10:00[Australia/Sydney]"),
+            dawn=ZonedDateTime.parse("2021-09-25T15:19:32.279+10:00[Australia/Sydney]"),
+            dusk=ZonedDateTime.parse("2021-09-26T04:27:40.269+10:00[Australia/Sydney]"),
+            nauticalDawn=ZonedDateTime.parse("2021-09-25T14:40:20.870+10:00[Australia/Sydney]"),
+            nauticalDusk=ZonedDateTime.parse("2021-09-26T05:06:51.678+10:00[Australia/Sydney]"),
+            nightEnd=ZonedDateTime.parse("2021-09-25T13:59:43.486+10:00[Australia/Sydney]"),
+            night=ZonedDateTime.parse("2021-09-26T05:47:29.061+10:00[Australia/Sydney]"),
+            goldenHourEnd=ZonedDateTime.parse("2021-09-25T16:36:54.771+10:00[Australia/Sydney]"),
+            goldenHour=ZonedDateTime.parse("2021-09-26T03:10:17.776+10:00[Australia/Sydney]"),
+            azimuth=-1.48815118586359, altitude=0.04749226792696052))
+
     override fun doWork(): Result
     {
         startLocationService()
 
-        //#Section ------------
-      //  uvDataDeferred = deferred()
-
-        val test = UVData(uv=0.0399F, uvTime= ZonedDateTime.parse("2021-09-25T00:00:30.826+10:00[Australia/Sydney]"),
-            uvMax=3.0005F, uvMaxTime= ZonedDateTime.parse("2021-09-25T21:53:36.274+10:00[Australia/Sydney]"),
-            ozone=332.5F, ozoneTime= ZonedDateTime.parse("2021-09-25T16:04:07.137+10:00[Australia/Sydney]"),
-            safeExposure= mapOf("st1" to 4180, "st2" to 5016, "st3" to 6688, "st4" to 8360, "st5" to 13376, "st6" to 25079),
-                sunInfo=SunInfo(solarNoon= ZonedDateTime.parse("2021-09-25T21:53:36.274+10:00[Australia/Sydney]"), nadir=ZonedDateTime.parse("2021-09-25T09:53:36.274+10:00[Australia/Sydney]"),
-                    sunrise=ZonedDateTime.parse("2021-09-25T15:52:48.317+10:00[Australia/Sydney]"),
-                    sunset=ZonedDateTime.parse("2021-09-26T03:54:24.230+10:00[Australia/Sydney]"),
-                    sunriseEnd=ZonedDateTime.parse("2021-09-25T15:56:13.870+10:00[Australia/Sydney]"),
-                    sunsetStart=ZonedDateTime.parse("2021-09-26T03:50:58.677+10:00[Australia/Sydney]"),
-                    dawn=ZonedDateTime.parse("2021-09-25T15:19:32.279+10:00[Australia/Sydney]"),
-                    dusk=ZonedDateTime.parse("2021-09-26T04:27:40.269+10:00[Australia/Sydney]"),
-                    nauticalDawn=ZonedDateTime.parse("2021-09-25T14:40:20.870+10:00[Australia/Sydney]"),
-                    nauticalDusk=ZonedDateTime.parse("2021-09-26T05:06:51.678+10:00[Australia/Sydney]"),
-                    nightEnd=ZonedDateTime.parse("2021-09-25T13:59:43.486+10:00[Australia/Sydney]"),
-                    night=ZonedDateTime.parse("2021-09-26T05:47:29.061+10:00[Australia/Sydney]"),
-                    goldenHourEnd=ZonedDateTime.parse("2021-09-25T16:36:54.771+10:00[Australia/Sydney]"),
-                    goldenHour=ZonedDateTime.parse("2021-09-26T03:10:17.776+10:00[Australia/Sydney]"),
-                    azimuth=-1.48815118586359, altitude=0.04749226792696052))
-
-                       // uvDataDeferred?.resolve(test)
-    //#Section ------------------
-            //        NetworkRepository.Semi_OLDgetRealTimeUV().success()
-            //        { data ->
-            //            uvDataDeferred?.resolve(data)
-            //        }
-
-    //#Section --------------------
         return Result.success()
     }
 
@@ -326,7 +316,7 @@ class UVDataWorker(applicationContext: Context, workerParameters: WorkerParamete
 
         uvDataDeferred = deferred()
 
-        if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
             //locationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
 
