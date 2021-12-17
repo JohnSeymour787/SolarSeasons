088878272e8ce5fd39dd274d1c9923ae0f2c7f09
John Seymour <john.seymour787@gmail.com>
2021-12-17 13:43:09 +1100

    Added SunInfo view cell and layout

---
+++ app/src/main/java/com/johnseymour/solarseasons/SunInfoViewCell.kt
@@ -0,0 +1,11 @@
+package com.johnseymour.solarseasons
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class SunInfoViewCell(context: Context, attrs: AttributeSet? = null): ConstraintLayout(context, attrs)
+{
+    init { (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as? LayoutInflater)?.inflate(R.layout.list_cell_sun_info, this, true) }
+}
