f40b6631081f2f25cf79a7c619b38a45c18e0806
John Seymour <john.seymour787@gmail.com>
2021-12-22 12:20:57 +1100

    Added user friendly exposure time strings

--- app/src/main/java/com/johnseymour/solarseasons/SkinExposureAdapter.kt
+++ app/src/main/java/com/johnseymour/solarseasons/SkinExposureAdapter.kt
@@ -15,7 +15,7 @@ class SkinExposureAdapter(private val exposureTimes: List<Map.Entry<String, Int>
             {
                 skinIcon.drawable.setTint(resources.getColor(UVData.skinTypeColorInt(exposureTimeEntry.key), context.theme))
                 skinType.text = resources.getString(UVData.skinTypeNameInt(exposureTimeEntry.key))
-                exposureTime.text = exposureTimeEntry.value.toString()//preferredTimeString(context, sunTime.second)
+                exposureTime.text = exposureDurationString(resources, exposureTimeEntry.value)
             }
         }
     }
