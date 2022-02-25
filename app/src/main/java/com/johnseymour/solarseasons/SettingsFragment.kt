ef7c94273e8dc431dd50e2463ded3d47fe745348
John Seymour <john.seymour787@gmail.com>
2022-02-25 16:21:35 +1100

    Added background explaination and force stop background processes button to SettingsFragment.kt

--- app/src/main/java/com/johnseymour/solarseasons/SettingsFragment.kt
+++ app/src/main/java/com/johnseymour/solarseasons/SettingsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment()
 {
@@ -20,6 +22,15 @@ class SettingsFragment : Fragment()
         childFragmentManager.setFragmentResultListener(PreferenceScreenFragment.PREFERENCES_UPDATED_FRAGMENT_RESULT_KEY, this)
         { _, bundle ->
             parentFragmentManager.setFragmentResult(PreferenceScreenFragment.PREFERENCES_UPDATED_FRAGMENT_RESULT_KEY, bundle)
+        }
+
+        stopBackgroundWorkButton.setOnClickListener()
+        {
+            val lApplicationContext = requireContext().applicationContext
+            UVDataWorker.cancelWorker(lApplicationContext)
+            UVDataWorker.stopLocationService(lApplicationContext)
+
+            Toast.makeText(lApplicationContext, R.string.settings_fragment_stop_background_success, Toast.LENGTH_LONG).show()
         }
     }
 
