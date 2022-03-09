18db0336c70f674d093ff0762e4c167fed5d3a9e
John Seymour <john.seymour787@gmail.com>
2022-03-09 13:11:49 +1100

    Updated error screen to follow theme set

--- app/src/main/java/com/johnseymour/solarseasons/CurrentUVFragment.kt
+++ app/src/main/java/com/johnseymour/solarseasons/CurrentUVFragment.kt
@@ -302,8 +302,11 @@ class CurrentUVFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener, Obse
     {
         appStatusInformation.text = errorStatus.statusString(resources)
         appStatusInformation.visibility = View.VISIBLE
-        layout.setBackgroundColor(resources.getColor(R.color.uv_low, requireContext().theme))
-        settingsButton.imageTintList = ColorStateList.valueOf(resources.getColor(R.color.dark_text, requireContext().theme))
+
+        if (PreferenceScreenFragment.useCustomTheme)
+        {
+            layout.setBackgroundColor(resources.getColor(R.color.uv_low, requireContext().theme))
+        }
 
         uvValue.visibility = View.INVISIBLE
         uvText.visibility = View.INVISIBLE
