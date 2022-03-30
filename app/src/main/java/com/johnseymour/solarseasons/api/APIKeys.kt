b72eebff1a9c72f452ab8937c80fb04a01c7ae25
John Seymour <48575153+JohnSeymour787@users.noreply.github.com>
2022-03-30 16:43:22 +1100

    Added variable API key for user-entered API key feature

--- app/src/main/java/com/johnseymour/solarseasons/api/APIKeys.kt
+++ app/src/main/java/com/johnseymour/solarseasons/api/APIKeys.kt
@@ -1,3 +1,3 @@
 package com.johnseymour.solarseasons.api
-
-const val OPENUV_API_KEY = "insert_api_key_here"
+// Can be changed if API_KEY_ENTRY_FEATURE is enabled
+var OPENUV_API_KEY = "insert_api_key_here"
