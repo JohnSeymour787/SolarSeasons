708e297fce65779bc9adf959576319373bf80617
John Seymour <john.seymour787@gmail.com>
2023-11-25 20:55:40 +1100

    Added support for persisting notifications

--- app/src/main/java/com/johnseymour/solarseasons/models/UVProtectionTimeData.kt
+++ app/src/main/java/com/johnseymour/solarseasons/models/UVProtectionTimeData.kt
@@ -29,6 +29,10 @@ data class UVProtectionTimeData(val fromTime: ZonedDateTime, val fromUV: Float, 
                 putExtra(PROTECTION_NOTIFICATION_BODY_KEY, context.getString(R.string.uv_protection_notification_start_protection_body_scheduled, preferredTimeString(context, fromTime), preferredTimeString(context, toTime), fromUV))
             }
         }
+
+        val persistNotificationIntent = (intent.clone() as Intent).apply { putExtra(PROTECTION_NOTIFICATION_PERSIST_NOTIFICATION_TIMEOUT_KEY, toTime) }
+        intent.putExtra(PROTECTION_NOTIFICATION_PERSIST_NOTIFICATION_KEY, persistNotificationIntent)
+
         return PendingIntent.getBroadcast(context, PROTECTION_START_REQUEST_CODE, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
     }
 
@@ -50,5 +54,7 @@ data class UVProtectionTimeData(val fromTime: ZonedDateTime, val fromUV: Float, 
         const val PROTECTION_NOTIFICATION_ID_KEY = "protection_notification_id_key"
         const val PROTECTION_NOTIFICATION_TITLE_KEY = "protection_notification_title_key"
         const val PROTECTION_NOTIFICATION_BODY_KEY = "protection_notification_body_key"
+        const val PROTECTION_NOTIFICATION_PERSIST_NOTIFICATION_KEY = "protection_notification_persist_notification_key"
+        const val PROTECTION_NOTIFICATION_PERSIST_NOTIFICATION_TIMEOUT_KEY = "protection_notification_persist_notification_timeout_key"
     }
 }
