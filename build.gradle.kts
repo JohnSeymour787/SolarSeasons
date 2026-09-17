adac777de081caa88f1146675cdab6944ebdb47e
john <john.seymour787@gmail.com>
2026-09-17 20:32:35 +1000

    Updated gradle to 9.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion by extra("2.0.21")
+    val kotlinVersion = "2.4.20"
+    extra.set("kotlinVersion", kotlinVersion)
 
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.android.tools.build:gradle:9.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
